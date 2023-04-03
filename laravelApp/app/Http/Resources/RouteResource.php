2d3757996d85f776bc8cf80ec24fce26129b1ba4
Thien Bao <truongbao901@gmail.com>
2023-04-03 21:37:14 +0700

    fix bus, fix validate

--- laravelApp/app/Http/Resources/RouteResource.php
+++ laravelApp/app/Http/Resources/RouteResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\Base;
 use App\Models\Bus;
+use App\Models\Seat;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,20 +19,27 @@ class RouteResource extends JsonResource
     public function toArray($request)
     {
         $bus = Bus::find($this->bus);
+        $seat = Seat::where('busId', $bus->id)->get();
+        $buss = [
+            'id' => $bus->id,
+            'bus_number' => $bus->plates,
+            'type' => $bus->type,
+            'seat' => $seat
+        ];
         $fromId = Base::find($this->fromId);
         $toId = Base::find($this->toId);
-    return [
-        'id' => $this->id,
-        'fromId' => $fromId,
-        'toId' => $toId,
-        'departure' => $this->departure,
-        'arrival' => $this->arrival,
-        'bus' => $bus,
-        'driver' => $this->driver,
-        'extraStaff' => $this->extraStaff,
-        'price' => $this->price,
-        'status' => $this->status,
-    ];
-}
+        return [
+            'id' => $this->id,
+            'fromId' => $fromId,
+            'toId' => $toId,
+            'departure' => $this->departure,
+            'arrival' => $this->arrival,
+            'bus' => $buss,
+            'driver' => $this->driver,
+            'extraStaff' => $this->extraStaff,
+            'price' => $this->price,
+            'status' => $this->status,
+        ];
+    }
 
 }
